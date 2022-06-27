c71b6678cca21b5d5bf8aae7ac6d153ca4e704a6
Yuuki <71581598+phu632003@users.noreply.github.com>
2022-06-27 09:31:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
 for i in range(len(cogs)):
 	cogs[i].setup(client)
 
-client.run("OTE3MzI1MjY0ODAxOTAyNjIz.Ya3Dug.QcU6QpcnaTaLM8piF13Yl1lZKYM")
+client.run
